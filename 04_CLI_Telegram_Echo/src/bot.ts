dbf3daa1fff7330cc66f9dfe3a846cc1fb9bf5b5
Oleksandr <alexandersemenuk212@gmail.com>
2024-02-22 19:24:52 +0200

    Telegram Echo added

---
+++ 04_CLI_Telegram_Echo/src/bot.ts
@@ -0,0 +1,30 @@
+import TelegramBot from "node-telegram-bot-api";
+import axios from "axios";
+
+
+const token = "token";
+
+const bot = new TelegramBot(token, {polling: true});
+
+async function getPhoto() {
+    const response = await axios.get('https://picsum.photos/200/300', {
+        responseType: 'arraybuffer'
+    });
+    return response.headers.location;
+}
+bot.on('message', async (msg)=> {
+    const chatId = msg.chat.id;
+    const messageReceived = msg.text;
+
+    console.log(`Message received from ${msg.from.first_name}: ${messageReceived}`);
+
+    if (messageReceived.toLowerCase() === 'photo') {
+          const response = await axios.get('https://picsum.photos/200/300', {
+            responseType: 'arraybuffer'
+        });
+        const photoUrl = Buffer.from(response.data, "binary");
+        bot.sendPhoto(chatId, photoUrl);
+      } else {
+        await bot.sendMessage(chatId, `You said: ${msg.text}`);
+      }
+});
