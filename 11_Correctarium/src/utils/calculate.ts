e4638c46c724422d2bf6c186438249a868bfa939
Oleksandr <alexandersemenuk212@gmail.com>
2024-04-06 14:49:32 +0300

    Task 11 deadlines fixed

--- 11_Correctarium/src/utils/calculate.ts
+++ 11_Correctarium/src/utils/calculate.ts
@@ -2,27 +2,70 @@ import moment from "moment-timezone";
 import { RequestBody, ResponseData } from "../types/types.js";
 
 export function calculateResponse(requestBody: RequestBody): ResponseData {
-    const {language, mimetype, count} = requestBody;
+    const { language, mimetype, count } = requestBody;
 
     const symbolsPerHour = language === 'en' ? 333 : 1333;
     const globalMultiplier = mimetype === 'other' ? 1.2 : 1;
-    const taskTimeInHours = Math.floor(count / symbolsPerHour) * globalMultiplier;
-    const currentTime = moment().tz('Europe/Kiev');
-    let deadlineTime = currentTime.add(Math.floor(count / symbolsPerHour) * 60, 'minutes');
-    const deadline = deadlineTime.unix();
-    const deadlineDate = deadlineTime.format('DD/MM/YYYY HH:mm:ss');
+    const taskTime = Math.floor((count / symbolsPerHour) * globalMultiplier) * 60;
+    const deadline = calculateDeadline(taskTime);
     const startingPrice = language === 'en' ? 120 : 50;
-    const pricePerSymbol = language === 'en' ? 0.12 : 0.06;
+    const pricePerSymbol = language === 'en' ? 0.12 : 0.05;
     const finalPrice = (startingPrice + (count * pricePerSymbol)) * globalMultiplier;
     
     const responseData: ResponseData = { 
         price: finalPrice,
-        time: taskTimeInHours,
-        deadline: deadline,
-        'deadline_date': deadlineDate,
+        time: taskTime / 60,
+        deadline: Math.floor(deadline.getTime() / 1000),
+        'deadline_date': deadline.toLocaleString(),
     };
 
     return responseData;
-
-    
 }
+
+export function calculateDeadline(taskDurationInMinutes: number, now: Date = new Date()) {
+    let durationLeft = taskDurationInMinutes;
+  
+    const startOfWorkDay = 10;
+    const endOfWorkDay = 19;
+    const workDayDurationInMinutes = (endOfWorkDay - startOfWorkDay) * 60;
+  
+    let deadline = new Date(now);
+  
+    if (now.getDay() === 0) {
+      deadline.setDate(deadline.getDate() + 1);
+    } else if (now.getDay() === 6) {
+      deadline.setDate(deadline.getDate() + 2);
+    } else if (now.getHours() < startOfWorkDay) {
+      deadline.setHours(startOfWorkDay, 0, 0, 0);
+    } else if (now.getHours() >= endOfWorkDay || (now.getHours() === endOfWorkDay && now.getMinutes() > 0)) {
+      deadline.setDate(deadline.getDate() + 1);
+      deadline.setHours(startOfWorkDay, 0, 0, 0);
+    } else {
+      let currentDayWorkTimeLeft = endOfWorkDay * 60 - (deadline.getHours() * 60 + deadline.getMinutes());
+      if (durationLeft <= currentDayWorkTimeLeft) {
+        deadline.setMinutes(deadline.getMinutes() + durationLeft);
+      } else {
+        durationLeft -= currentDayWorkTimeLeft;
+        deadline.setDate(deadline.getDate() + 1);
+        deadline.setHours(startOfWorkDay, 0, 0, 0);
+      }
+    }
+  
+    while (durationLeft !== 0) {
+      if (deadline.getDay() === 6) {
+        deadline.setDate(deadline.getDate() + 2);
+        deadline.setHours(startOfWorkDay, 0, 0, 0);
+      }
+  
+      if (durationLeft <= workDayDurationInMinutes) {
+        deadline.setMinutes(deadline.getMinutes() + durationLeft);
+        durationLeft -= durationLeft;
+      } else {
+        deadline.setDate(deadline.getDate() + 1);
+        deadline.setHours(startOfWorkDay, 0, 0, 0);
+        durationLeft -= 540;
+      }
+    }
+  
+    return deadline;
+  }
